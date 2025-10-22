c00164b1273b537278787b6bc3d3893ed0b06142
NITIN SAI PATELLA <nitinsaipatella2024@gmail.com>
2025-10-22 21:21:32 +0530

    final chnages

--- hanuman_scene/server.py
+++
@@ -1,7 +0,0 @@
-from http.server import SimpleHTTPRequestHandler, HTTPServer
-
-PORT = 8000
-
-with HTTPServer(("localhost", PORT), SimpleHTTPRequestHandler) as httpd:
-    print(f"🚀 Server running at http://localhost:{PORT}")
-    httpd.serve_forever()
